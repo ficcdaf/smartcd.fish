fff0b55728770161ab8dd322f57c6d39e78c29c4
Daniel Fichtinger <daniel@ficd.ca>
2025-04-18 16:04:02 -0400

    added help, prompt for leaving root, and update function

--- README.md
+++ README.md
@@ -6,20 +6,39 @@
 
 `smartcd` makes `cd` smarter when used without arguments. It adapts based on
 your current location, helping you more around Git repos and your system more
-efficiently.
+efficiently. It works inside the [fish](https://fishshell.com/) shell.
 
 ## Usage
 
-When you run `cd` with **no arguments**:
+The command is accessible as `cd`:
 
-- If **inside a Git repository**:
-  - Jumps to the **repository root**.
-  - If **already at the root**, jumps to the **previous directory**.
-- If **outside a Git repository**:
-  - Jumps to the **home directory**.
-  - If **already at home**, jumps to the **previous directory**.
+```fish
+# arguments work as before
+cd /foo/bar
+# including - to go backwards manually
+cd -
+# running cd _without_ arguments
+# triggers the smart behavior
+cd
+# help is also available
+cd --help
+```
 
-This enables a kind of "bouncing" navigation.
+- `ROOT`:
+  - Repository root if **inside** a Git repository.
+  - `$HOME` if **_not_ inside** a Git repository.
+- `PREV`:
+  - Your _previous_ directory.
+  - Last element of `$dirprev`.
+
+When you invoke `cd` without arguments:
+
+- If not at `ROOT`, return to `ROOT`.
+- If _already_ at `ROOT`, return to `PREV`.
+- If `PREV` is _not_ a child of `ROOT`, prompt before jumping.
+
+An update function, `smartcd_update`, is provided. It uses `curl` to save the
+latest version of `smartcd.fish` to `$fish_config/conf.d`.
 
 ## Installation
 
@@ -30,6 +49,19 @@ To install, copy [smartcd.fish](./smartcd.fish) to your `conf.d` directory. Use
 curl https://git.sr.ht/~ficd/smartcd.fish/blob/main/smartcd.fish \
     -o $fish_config/conf.d/smartcd.fish
 exec fish
+```
+
+Once installed, `smartcd` provides an update function. You may invoke it at any
+time to update `smartcd` to the latest version. Pass `-d` or `--dry` to print
+the script to `STDOUT` without saving the file.
+
+```fish
+# Check connectivity & print script to STDOUT
+smartcd_update --dry
+# Update conf.d
+smartcd_update
+# View help
+smartcd_update --help
 ```
 
 ## License
--- smartcd.fish
+++ smartcd.fish
@@ -1,12 +1,30 @@
 status is-interactive; or exit
 
-# necessary because we want to use fish's
-# cd wrapper function, _not_ the builtin cd!
-if not functions -q __cd_orig
-    functions --copy cd __cd_orig
-end
+# Ensure original cd is saved once
+functions -q __cd_orig; or functions --copy cd __cd_orig
 
 function cd --wraps=cd --description "Change directory. No args goes to git root or $HOME. If already there, return to previous location."
+    argparse h/help -- $argv
+    if set -q _flag_help
+        echo smartcd.fish: Change directories, slightly smarter.
+        echo Invoke by running cd. Update with smartcd_update.\n
+        echo Usage:
+        echo -h/--help: show this menu.
+        echo cd [args]: behaves like normal cd.
+        echo cd [no-args]: cd to ROOT. If at ROOT, cd to PREV.\n
+        echo ROOT = Git repo root '||' "\$HOME".
+        echo PREV = Last element of "\$dirprev".
+        echo If PREV is outside ROOT, user is prompted first.\n
+        echo Author: Daniel Fichtinger '<daniel@ficd.ca>'
+        echo Upstream: https://git.sr.ht/~ficd/smartcd.fish
+        echo License: ISC
+        return 0
+    end
+    # Skip history in subshells.
+    if status --is-command-substitution
+        builtin cd $argv
+        return $status
+    end
     git rev-parse --is-inside-work-tree &>/dev/null
     set -l is_git $status
     if test (count $argv) -ne 0
@@ -19,8 +37,43 @@ function cd --wraps=cd --description "Change directory. No args goes to git root
         end
         if not test (pwd) = $root
             __cd_orig $root
-        else
-            __cd_orig -
+        else if set -q dirprev
+            if string match -q "$root*" "$dirprev[-1]"
+                __cd_orig -
+            else
+                set -l prompt "$dirprev[-1], is outside $root, are you sure? (y/n):"
+                while read --nchars 1 -l response --prompt-str="$prompt" or return 1
+                    # clear prompt
+                    printf "\033[1A\033[2K"
+                    switch $response
+                        case y Y
+                            __cd_orig -
+                            break
+                        case n N
+                            break
+                    end
+                end
+            end
         end
     end
+end
+
+function smartcd_update --description "Update smartcd.fish with the latest from the upstream."
+    argparse h/help d/dry -- $argv
+    if set -q _flag_help
+        echo Update smartcd.fish with the latest from upstream.\n
+        echo Usage:
+        echo -d/--dry to not save the file.
+        echo -h/--help to print this screen.\n
+        echo Author: Daniel Fichtinger '<daniel@ficd.ca>'
+        echo Upstream: https://git.sr.ht/~ficd/smartcd.fish
+        echo License: ISC
+        return 0
+    end
+    set -l url https://git.sr.ht/~ficd/smartcd.fish/blob/main/smartcd.fish
+    # set -l add "-o $fish_config/conf.d/smartcd.fish"
+    not set -q _flag_dry; and set -l add -o $fish_config/conf.d/smartcd.fish
+    curl $url $add
+    printf "\033[1A\033[2K"
+    exec fish
 end
